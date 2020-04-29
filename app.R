d7701d1da7fec4c04951af4a15716ee29456c6cb
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2020-04-29 13:36:44 -0700

    Editorial

--- app.R
+++ app.R
@@ -1,2 +1,26 @@
+##### Header #####
+#
+# Author:       Matthew H. Grinnell
+# Affiliation:  Pacific Biological Station, Fisheries and Oceans Canada (DFO)
+# Group:        Stock Assessment and Research, Quantitative Assessment Methods
+# Address:      3190 Hammond Bay Road, Nanaimo, BC, Canada, V9T 6N7
+# Contact:      e-mail: matt.grinnell@dfo-mpo.gc.ca | tel: 250.756.7055
+# Project:      Herring
+# Code name:    FIND (FIND Is Not Difficult)
+# Version:      1.0
+# Date started: Jan 08, 2019
+# Date edited:  Apr 22, 2020
+#
+# Overview:
+# Show herring spawn events within a given distance from a point.
+#
+# Requirements:
+# RStudio, various R packages, herring spawn index data, as well as land and
+# herring section shapefiles.
+#
+# Notes:
+# This is a Shiny web application; run it by clicking the 'Run App' button in
+# RStudio.
+
 # Run the application
 shinyApp(ui = ui, server = server)
